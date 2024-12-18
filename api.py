548d69bd3f46b8c64bd5eb626f76ae2bdcce857d
livooo8 <biktimirov.2008@mail.ru>
2024-12-18 12:21:11 +0300

    added stupid api functions

--- api.py
+++ api.py
@@ -1,6 +1,21 @@
+import random
+
+
 def get_word(topic):
-    pass
-def get_answer(word,quest):
-    pass
-def is_equal(word1,word2):
-    pass
+    list = ["Гарри Поттер", "Гермиона Грейнджер", "Рон Уизли", "Драко Малфой", "Невилл Долгопупс"]
+    return list[random.randint(0, len(list) - 1)]
+
+
+def get_answer(word, quest):
+    z = random.randint(0, 1)
+    if z == 1:
+        return True
+    else:
+        return False
+
+
+def is_equal(word1, word2):
+    if word1 == word2:
+        return True
+    else:
+        return False
