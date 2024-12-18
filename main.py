158e78dc862a96a978a426e46625c80159a91959
LirichOfficial <AAElfimov@stud.kpfu.ru>
2024-12-18 15:12:27 +0300

    Закончил примерное оформление раздела со статистикой

--- main.py
+++ main.py
@@ -23,12 +23,22 @@ async def start(message: types.Message):
     await message.answer("Выберите действие", reply_markup=markup)
 
 
+@dp.message_handler(lambda message: message.text == 'В начало')
+async def start(message: types.Message):
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = KeyboardButton("Статистика")
+    markup.add(item1)
+
+    await message.answer("Выберите действие", reply_markup=markup)
+
+
 @dp.message_handler(lambda message: message.text == 'Статистика')
-async def process_button1(message: types.Message):
+async def process_stat(message: types.Message):
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
     item2 = KeyboardButton("Лучшие игроки")
     item3 = KeyboardButton("Текущее положение")
-    markup.add(item2, item3)
+    item4 = KeyboardButton("В начало")
+    markup.add(item2, item3, item4)
 
 #    user = data.get_user_data(message.from_user.username)
     user = {"points":"1200", "Rank":"Expert"}
@@ -36,6 +46,42 @@ async def process_button1(message: types.Message):
     ans = message.from_user.username + ":\n" + "Очки: " + user["points"] + "\n" + "Звание: " + user["Rank"]
 
     await message.answer(ans, reply_markup=markup)
+
+
+
+@dp.message_handler(lambda message: message.text == 'Лучшие игроки')
+async def process_top10(message: types.Message):
+    #top = data.get_global_stats()
+    top = {"livoo":"1200", "amirkhan":"1175", "lenya":"1100"}
+
+    ans = ""
+    cnt = 0
+    
+    for i in top.items():
+        cnt = cnt + 1
+        ans = ans + "\n" + str(cnt) + ". " + i[0] + " - " + i[1]
+    await message.answer(ans)
+
+
+
+@dp.message_handler(lambda message: message.text == 'Текущее положение')
+async def process_top10(message: types.Message):
+    #top = data.get_global_stats()
+    top = {"livoo":"1200", "amirkhan":"1175", "lenya":"1100", "deelovaya_kolbasa":"1000"}
+
+    ans = ""
+    cnt = 0
+
+    for i in top.items():
+        cnt = cnt + 1
+        if (i[0] == message.from_user.username):
+            ans = ans + "\n->" + str(cnt) + ". " + i[0] + " - " + i[1] + "<-"
+        else:
+            ans = ans + "\n" + str(cnt) + ". " + i[0] + " - " + i[1]
+    await message.answer(ans)
+
+
+
 
 async def main():
     await dp.start_polling(bot)
