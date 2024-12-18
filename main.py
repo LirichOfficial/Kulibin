ac2f0dad788fa461a4c2b939aa295d5796e2071f
LirichOfficial <AAElfimov@stud.kpfu.ru>
2024-12-18 12:23:13 +0300

    Добавил пару функциональных кнопок, функционал и дизайн дорабатывается

--- main.py
+++ main.py
@@ -1,18 +1,44 @@
 import logging
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
-from aiogram import F
+from aiogram.dispatcher.filters import Command
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import data
 import asyncio
 import random
 
 
+API_TOKEN = '8147906166:AAGL6vWBWvZPwUnUzRy0HC6hKwvl43TEBHs'
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher()
+dp = Dispatcher(bot)
 
-def main():
-    pass
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = KeyboardButton("Статистика")
+    markup.add(item1)
 
-if "__name__" == main:
-    main()
+    await message.answer("Выберите действие", reply_markup=markup)
+
+
+@dp.message_handler(lambda message: message.text == 'Статистика')
+async def process_button1(message: types.Message):
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    item2 = KeyboardButton("Лучшие игроки")
+    item3 = KeyboardButton("Текущее положение")
+    markup.add(item2, item3)
+
+#    user = data.get_user_data(message.from_user.username)
+    user = {"points":"1200", "Rank":"Expert"}
+
+    ans = message.from_user.username + ":\n" + "Очки: " + user["points"] + "\n" + "Звание: " + user["Rank"]
+
+    await message.answer(ans, reply_markup=markup)
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
