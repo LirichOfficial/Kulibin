868247ea431c1621ed95345e71bd400837d4b98d
Aviat9181 <ilnurrz116@gmail.com>
2024-12-18 14:51:06 +0300

    modified:   data.py 	modified:   games.json 	modified:   stats.json 	new file:   users.json Функция добавления новых игр теперь работает

--- data.py
+++ data.py
@@ -1,31 +1,47 @@
 import json
 import os
 
-def get_user_data(user):
-    with open('stats.json') as dtb:
-        db=json.load(dtb)
-    return db['user']
+#def get_user_data(user):
+    #with open('stats.json') as dtb:
+    #    db=json.load(dtb)
+    #return db['user']
 
 
 def add_new_game(user,points,date,theme,answer):
-    with open('games.json') as dtb:
-        db_games=json.load(dtb)
-    with open('stats.json') as dtb:
-        db_stats=json.load(dtb)
+    db_games=json.load(open('games.json'))
+    db_stats=json.load(open('stats.json'))
 
-    value={}
-    value['points']=points
-    value['date']=date
-    value['theme']=theme
-    value['answer']=answer
+    game_value={}
+    game_value['points']=points
+    game_value['date']=date
+    game_value['theme']=theme
+    game_value['answer']=answer
 
-    db_games[user].append(value)
-    db_stats[user]['points']+=points
-    db_stats[user]['games_count']+=1
-    db_stats[user]['avg']=db_stats[user]['points']/db_stats[user]['games_count']
+    stat_value={}
+    stat_value['points']=points
+    stat_value['games_count']=1
+    stat_value['avg']=points
+
+    if not user in db_games:
+        db_games[user]=[game_value]
+    else:
+        stat_value['points']+=db_stats[user]['points']
+        stat_value['games_count']+=db_stats[user]['games_count']
+        stat_value['avg']=stat_value['points']/stat_value['games_count']
+        db_games[user].append(game_value)
+    
+    db_stats[user]=stat_value
+    with open('stats.json','w') as file:
+        json.dump(db_stats,file)
+    with open('games.json','w') as file:
+        json.dump(db_games,file)
 
 
 def get_global_stats():
-    pass
+    answer=[]
+
 def get_user_in_global_stats(user):
     pass
+
+add_new_game('gay',52,-1,'penis','anus')
+#get_global_stats()
