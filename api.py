88572c36777bcb3ffbde031f3efa7829722972e6
Aviat9181 <ilnurrz116@gmail.com>
2024-12-18 10:48:56 +0300

    Changes to be committed: 	new file:   api.py 	new file:   backup.json 	new file:   data.py 	new file:   database.json 	new file:   main.py base functions

---
+++ api.py
@@ -0,0 +1,6 @@
+def get_word(topic):
+    pass
+def get_answer(word,quest):
+    pass
+def is_equal(word1,word2):
+    pass
---
+++ data.py
@@ -0,0 +1,8 @@
+def get_used_data(user):
+    pass
+def add_new_game(use,points,date,theme,ans):
+    pass
+def get_global_stats():
+    pass
+def get_user_in_global_stats(used):
+    pass
