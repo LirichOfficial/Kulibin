f0d89613bd04b63e0081ff47cb73b14ab17d1bd1
LirichOfficial <AAElfimov@stud.kpfu.ru>
2024-12-18 10:56:49 +0300

    Added base things

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+from aiogram import F
+import asyncio
+import random
+
+
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
 def main():
     pass
 
